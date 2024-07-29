7596ae5a677239034766c59a0723462d151ccc1a
Diogo <dapereira@inf.ufsm.br>
2024-07-29 13:13:06 -0300

    a

--- django/rifa/urls.py
+++ django/rifa/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("rifa/<int:rifa_id>/", views.rifa, name="rifa"),
+    path("rifa/<int:rifa_id>/detalhes/<int:numero_id>", views.detalhes_numero, name="numero_detail"),
+    path("rifa/<int:rifa_id>/comprar/<int:numero_id>", views.comprar_numero, name="comprar_numero"),
 ]
--- django/rifa/views.py
+++ django/rifa/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 from .models import Rifa, NumeroVendido
+from config.models import Conta
+import base64
+from io import BytesIO
+from PIL import Image
+
 def index(request):
     return render(request, "rifa/index.html")
 
@@ -10,3 +15,24 @@ def rifa(request, rifa_id):
     numeros = NumeroVendido.objects.filter(rifa_relacionada=rifa)
     print(numeros)
     return render(request, "rifa/rifa.html", {"rifa": rifa, "Numeros": numeros})
+
+def detalhes_numero(request, rifa_id, numero_id):
+    rifa = Rifa.objects.get(id=rifa_id)
+
+    numero = NumeroVendido.objects.get(id=numero_id, rifa_relacionada=rifa)
+    print(numero)
+    print(rifa)
+    return render(request, 'rifa/numero_detail.html', {'numero': numero, 'rifa': rifa})
+
+def comprar_numero(request, rifa_id, numero_id):
+    rifa = Rifa.objects.get(id=rifa_id)
+
+    numero = NumeroVendido.objects.get(id=numero_id, rifa_relacionada=rifa)
+    print(numero)
+    print(rifa)
+
+    print("TAMO NO COMPRAR NUMERO")
+
+    conta = Conta.objects.get(nome='Rifas')
+
+    return render(request, 'rifa/pagamento.html', {'numero': numero, 'rifa': rifa, 'qr_code': conta.gerar_pix(str(rifa.valor_numero))})
