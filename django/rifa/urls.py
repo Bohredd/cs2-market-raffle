14d62c3276d2e5d986f866d253df1432659993e1
Diogo <dapereira@inf.ufsm.br>
2024-08-02 10:15:32 -0300

    feat: reservar

--- django/rifa/urls.py
+++ django/rifa/urls.py
@@ -1,9 +1,10 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("rifa/<int:rifa_id>/", views.rifa, name="rifa"),
-    path("rifa/<int:rifa_id>/detalhes/<int:numero_id>", views.detalhes_numero, name="numero_detail"),
+    re_path(r'^rifa/(?P<rifa_id>\d+)/(?P<refresh>\d+)?$', views.rifa, name='rifa'),
+    path("rifa/detalhes/<int:numero_id>", views.detalhes_numero, name="numero_detail"),
     path("rifa/<int:rifa_id>/comprar/<int:numero_id>", views.comprar_numero, name="comprar_numero"),
+    path('rifa/reservar/<int:numero_id>', views.reservar_numero, name='reservar_numero'),
 ]
--- django/rifa/views.py
+++ django/rifa/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Rifa, NumeroVendido
 from config.models import Conta
 import base64
@@ -8,31 +8,37 @@ from PIL import Image
 def index(request):
     return render(request, "rifa/index.html")
 
-def rifa(request, rifa_id):
+def rifa(request, rifa_id, refresh=None):
+    rifa = get_object_or_404(Rifa, id=rifa_id)
     rifa = Rifa.objects.get(id=rifa_id)
     rifa.save()
-    print(rifa.numeros.all())
     numeros = NumeroVendido.objects.filter(rifa_relacionada=rifa)
-    print(numeros)
-    return render(request, "rifa/rifa.html", {"rifa": rifa, "Numeros": numeros})
+    
+    if refresh is None:
+        refresh = 0
 
-def detalhes_numero(request, rifa_id, numero_id):
-    rifa = Rifa.objects.get(id=rifa_id)
+    return render(request, "rifa/rifa.html", {"rifa": rifa, "Numeros": numeros, 'refresh': refresh})
 
-    numero = NumeroVendido.objects.get(id=numero_id, rifa_relacionada=rifa)
-    print(numero)
-    print(rifa)
-    return render(request, 'rifa/numero_detail.html', {'numero': numero, 'rifa': rifa})
+def detalhes_numero(request, numero_id):
+
+    numero = NumeroVendido.objects.get(id=numero_id)
+
+    return render(request, 'rifa/numero_detail.html', {'numero': numero, 'rifa': numero.rifa_relacionada})
 
 def comprar_numero(request, rifa_id, numero_id):
     rifa = Rifa.objects.get(id=rifa_id)
 
     numero = NumeroVendido.objects.get(id=numero_id, rifa_relacionada=rifa)
-    print(numero)
-    print(rifa)
-
-    print("TAMO NO COMPRAR NUMERO")
 
     conta = Conta.objects.get(nome='Rifas')
 
     return render(request, 'rifa/pagamento.html', {'numero': numero, 'rifa': rifa, 'qr_code': conta.gerar_pix(str(rifa.valor_numero))})
+
+def reservar_numero(request, numero_id):
+
+    numero = NumeroVendido.objects.get(id=numero_id)
+
+    numero.status = 'R'
+    numero.save()
+
+    return redirect('rifa', rifa_id=numero.rifa_relacionada.id, refresh=1)
